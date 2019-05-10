de18a0af0ca5ae4027bfea29df6eca624730e8e4
Lucas Sartor <lucassartorchauvin@gmail.com>
2019-05-10 09:35:09 -0300

    Table changes, coordinator table reframed and initial user configs
    Big vitor´s class
    09-05-19

---
+++ database/migrations/2019_05_10_090650_create_sectors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sectors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('nome', 50)->nullable(false);
+            $table->text('descricao');
+            $table->boolean('ativo', 100)->nullable(false)->default(true);
+
+            $table->bigInteger('id_company')->nullable(false)->unsigned();
+            $table->foreign('id_company')->references('id')->on('companies')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sectors');
+    }
+}
---
+++ database/migrations/2019_05_10_091555_create_agreements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgreementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agreements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('id_company')->nullable(false)->unsigned();
+            $table->foreign('id_company')->references('id')->on('companies')->onDelete('cascade');
+
+            $table->date('validade')->nullable(false);
+            $table->text('observacao')->nullable(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agreements');
+    }
+}

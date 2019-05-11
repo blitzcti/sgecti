d6bcbd0cf18bb3fcf0ed8daea826d5c38fd5fe67
Dhiego Cassiano Fogaça Barbosa <modscleo4@outlook.com>
2019-05-11 08:27:22 -0300

    Base for User CRUD

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index() {
+
+    }
+
+    public function new() {
+
+    }
+
+    public function edit($id) {
+
+    }
+
+    public function save(Request $request) {
+
+    }
+}

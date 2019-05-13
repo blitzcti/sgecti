fb21e8746aa8e22dca5ed89869a17eab7ae7f073
Dhiego Cassiano Fogaça Barbosa <modscleo4@outlook.com>
2019-05-13 15:07:53 -0300

    Base for Coordinator CRUD

---
+++ app/Http/Controllers/CoordinatorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CoordinatorController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function new()
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function save(Request $request)
+    {
+
+    }
+}
